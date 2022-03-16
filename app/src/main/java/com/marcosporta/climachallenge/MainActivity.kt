fb31cf4f6daf6750e0e7ccf83735874cd18ad14c
MarcosPorta <marcosporta15@gmail.com>
2022-03-16 18:38:26 -0300

    Creacion y paso a Activity HomePage

--- app/src/main/java/com/marcosporta/climachallenge/MainActivity.kt
+++ app/src/main/java/com/marcosporta/climachallenge/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.marcosporta.climachallenge
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+    //Tratamiento del login
+    fun clickLogin(view: View){
+
+        val intent= Intent(this,HomePage::class.java)
+        startActivity(intent)
+    }
+
 }
