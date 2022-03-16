a07958818f37504e540320e8fc48308b7ef99148
MarcosPorta <marcosporta15@gmail.com>
2022-03-16 19:03:23 -0300

    Diseño HomePage

--- app/src/main/java/com/marcosporta/climachallenge/MainActivity.kt
+++ app/src/main/java/com/marcosporta/climachallenge/MainActivity.kt
@@ -4,8 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var usuarioTV: EditText
+    lateinit var passwordTV: EditText
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,7 +22,9 @@ class MainActivity : AppCompatActivity() {
 
     //Tratamiento del login
     fun clickLogin(view: View){
-
+        usuarioTV = findViewById(R.id.etUsuario)
+        passwordTV = findViewById(R.id.etPassword)
+        println("MIRAR ACA --------> $usuarioTV")
         val intent= Intent(this,HomePage::class.java)
         startActivity(intent)
     }
